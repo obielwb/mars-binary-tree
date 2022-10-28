e00d39855f809b98e5d255ec5472b347518cf02a
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-10-28 15:29:48 -0300

    feat: add Caminho class

---
+++ Caminho.cs
@@ -0,0 +1,80 @@
+﻿// Gabriel Willian Bartmanovicz - 21234
+// João Pedro Ferreira Barbosa - 21687
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+class Caminho : IComparable<Caminho>, IRegistro
+{
+    private int codigoOrigem, codigoDestino, distancia, custo, tempo;
+
+    private const int tamanhoRegistro = sizeof(int) + // código de origem
+                                        sizeof(int) + // código de destino
+                                        sizeof(int) + // distância
+                                        sizeof(int) + // custo
+                                        sizeof(int); // tempo
+
+    public int CodigoOrigem { get => codigoOrigem; set => codigoOrigem = value; }
+    public int CodigoDestino { get => codigoDestino; set => codigoDestino = value; }
+    public int Distancia { get => distancia; set => distancia = value; }
+    public int Custo { get => custo; set => custo = value; }
+    public int Tempo { get => tempo; set => tempo = value; }
+    public int TamanhoRegistro { get => tamanhoRegistro; }
+
+    public Caminho()
+    { }
+
+    public Caminho(int codigoOrigem, int codigoDestino, int distancia, int custo, int tempo)
+    {
+        CodigoOrigem = codigoOrigem;
+        CodigoDestino = codigoDestino;
+        Distancia = distancia;
+        Custo = custo;
+        Tempo = tempo;
+    }
+
+    public int CompareTo(Caminho outro)
+    {
+        return (CodigoOrigem.ToString() + CodigoDestino.ToString()).CompareTo(outro.CodigoOrigem.ToString() + outro.CodigoDestino.ToString());
+    }
+
+    public override string ToString()
+    {
+        return CodigoOrigem + " " + CodigoDestino + " " + Distancia + " " + Custo + " " + Tempo;
+    }
+
+    public void LerRegistro(BinaryReader arquivo, long registro)
+    {
+        if (arquivo != null)
+        {
+            try
+            {
+                long bytes = registro * TamanhoRegistro;
+                arquivo.BaseStream.Seek(bytes, SeekOrigin.Begin);
+
+                CodigoOrigem = arquivo.ReadInt32();
+                CodigoDestino = arquivo.ReadInt32();
+                Distancia = arquivo.ReadInt32();
+                Custo = arquivo.ReadInt32();
+                Tempo = arquivo.ReadInt32();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+
+    public void GravarRegistro(BinaryWriter arquivo)
+    {
+        if (arquivo != null)
+        {
+            arquivo.Write(CodigoOrigem);
+            arquivo.Write(CodigoDestino);
+            arquivo.Write(Distancia);
+            arquivo.Write(Custo);
+            arquivo.Write(Tempo);
+        }
+    }
+}
--- Cidade.cs
+++ Cidade.cs
@@ -3,33 +3,35 @@
 
 using System;
 using System.IO;
+using System.Windows.Forms;
 
-class Cidadex : IComparable<Cidade>, IRegistro<Cidade>
+class Cidade : IComparable<Cidade>, IRegistro
 {
-    const int tamCodigo = 3,
-              tamNome = 15,
-              tamX = 7,
-              tamY = 7;
+    public const int tamanhoNome = 15;
 
-    const int iniCodigo = 0,
-              iniNome = iniCodigo + tamCodigo,
-              iniX = iniNome + tamNome,
-              iniY = iniX + tamX;
+    private int codigo;
+    private string nome;
+    private decimal x, y;
 
-    string codigo, nome;
-    decimal x, y;
+    private const int tamanhoRegistro = sizeof(int) + // código
+                                        tamanhoNome + // nome
+                                        sizeof(decimal) + // x
+                                        sizeof(decimal); // y
 
-    public string Codigo { get => codigo; set => codigo = value.PadLeft(tamCodigo, '0').Substring(0, tamCodigo); }
-    public string Nome { get => nome; set => nome = value.PadRight(tamCodigo, ' ').Substring(0, tamNome); }
-
-    // atributos decimais para as coordenadas 0.xxxxx
+    public int Codigo { get => codigo; set => codigo = value; }
+    public string Nome { get => nome; set => nome = value; }
     public decimal X { get => x; set => x = value; }
     public decimal Y { get => y; set => y = value; }
+    public int TamanhoRegistro { get => tamanhoRegistro; }
 
-    public Cidade() // construtor vazio para instância new Dado()
-    { }
+    public Cidade()
+    {
+        Codigo = 0;
+        Nome = "";
+        X = Y = 0;
+    }
 
-    public Cidade(string codigo, string nome, decimal x, decimal y)
+    public Cidade(int codigo, string nome, decimal x, decimal y)
     {
         Codigo = codigo;
         Nome = nome;
@@ -37,43 +39,63 @@ class Cidadex : IComparable<Cidade>, IRegistro<Cidade>
         Y = y;
     }
 
-    public int CompareTo(Cidade outro)
+    public Cidade(int codigo)
     {
-        return nome.ToUpperInvariant().CompareTo(outro.Nome.ToUpperInvariant());
+        Codigo = codigo;
     }
 
-    public Cidade LerRegistro(StreamReader arquivo)
-   {
-        if (arquivo != null) // arquivo aberto?
-        {
-            string linha = arquivo.ReadLine();
-
-            Codigo = linha.Substring(iniCodigo, tamCodigo);
-            Nome = linha.Substring(iniNome, tamNome);
-            X = decimal.Parse(linha.Substring(iniX, tamX));
-            Y = decimal.Parse(linha.Substring(iniY));
-
-            return this; // retorna o próprio objeto Cidade, com os dados
-        }
-
-        return default;
-    }
-
-    public void GravarRegistro(StreamWriter arquivo)
+    public int CompareTo(Cidade cidade)
     {
-        if (arquivo != null)  // arquivo de saída aberto?
-        {
-            arquivo.WriteLine(ParaArquivo());
-        }
-    }
-
-    public string ParaArquivo()
-    {
-        return Codigo + Nome + X.ToString("0.00000") + Y.ToString("0.00000"); // formata as coordenadas
+        return nome.ToUpperInvariant().CompareTo(cidade.Nome.ToUpperInvariant());
     }
 
     public override string ToString()
     {
-        return Codigo + " " + Nome + " " + X.ToString().PadLeft(tamX, ' ') + Y.ToString().PadLeft(tamY, ' ');
+        return Codigo + " " + Nome + " " + X.ToString().PadLeft(7, ' ') + Y.ToString().PadLeft(7, ' '); ;
+    }
+
+    public void LerRegistro(BinaryReader arquivo, long registro)
+    {
+        if (arquivo != null)
+        {
+            try
+            {
+                long bytes = registro * TamanhoRegistro;
+                arquivo.BaseStream.Seek(bytes, SeekOrigin.Begin);
+
+                Codigo = arquivo.ReadInt32();
+
+                char[] nome = new char[tamanhoNome];
+                nome = arquivo.ReadChars(tamanhoNome);
+
+                foreach (char letra in nome)
+                    Nome += letra;
+
+                X = arquivo.ReadDecimal();
+                Y = arquivo.ReadDecimal();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+
+    public void GravarRegistro(BinaryWriter arquivo)
+    {
+        if (arquivo != null)
+        {
+            arquivo.Write(Codigo);
+
+            char[] nome = new char[tamanhoNome];
+
+            for (int i = 0; i < tamanhoNome; i++)
+                nome[i] = Nome[i];
+
+            arquivo.Write(nome);
+
+            arquivo.Write(X);
+            arquivo.Write(Y);
+        }
     }
 }
